c73e1258e9bf815cf39052e8a7831c1a5cb0de84
DevTobias <tobi.kaerst@gmx.de>
2020-12-15 02:30:44 +0100

    basic react project structure

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Infotition</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
